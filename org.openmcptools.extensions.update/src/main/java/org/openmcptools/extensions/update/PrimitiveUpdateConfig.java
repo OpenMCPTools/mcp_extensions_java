e33eedd06362534ec4eafa44dabce41673c45ee3
Scott Lewis <scottslewis@gmail.com>
2026-02-20 14:28:54 -0800

    Added constants for update extension send/receive.

--- org.openmcptools.extensions.update/src/main/java/org/openmcptools/extensions/update/PrimitiveUpdateConfig.java
+++ org.openmcptools.extensions.update/src/main/java/org/openmcptools/extensions/update/PrimitiveUpdateConfig.java
@@ -2,11 +2,13 @@ package org.openmcptools.extensions.update;
 
 public class PrimitiveUpdateConfig {
 
-	public static String EXTENSION_ID = "org.openmcptools/update";
+	public static final String EXTENSION_ID = "org.openmcptools/update";
 
-	public static String SERVER_CAPABILITIES_ID = EXTENSION_ID + "/server";
-	public static String CLIENT_CAPABILITIES_ID = EXTENSION_ID + "/client";
+	public static final String SERVER_CAPABILITIES_ID = EXTENSION_ID + "/server";
+	public static final String CLIENT_CAPABILITIES_ID = EXTENSION_ID + "/client";
 
-	public static String NOTIFICATION_TOPIC = "notification/tools/updated";
+	public static final String NOTIFICATION_TOPIC = "notification/tools/updated";
+
+	public static final String PRIMITIVE_UPDATE_EVENTS_KEY = "primtiveUpdateEvents";
 
 }
